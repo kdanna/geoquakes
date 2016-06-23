c299ffa19880ca853bad704ecf8f992442ac478e
kdanna <kayce.danna@gmail.com>
2016-06-22 18:28:04 -0600

    geoquakes lab result

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,56 @@
-// define globals
+	// define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 var $quakesList;
 var map;
-var template;
+var source = $("#quakes-template").html();
+var template = Handlebars.compile(source);
+
+
+
 
 $(document).on("ready", function() {
+	
 
 
-});
+	$.get(weekly_quakes_endpoint, function(data){
+			
+		  console.log(data); // this works	
+		  var quakeNames = template({quakes: data.features});
+		  $("#info").append(quakeNames);
+
+
+
+		  var quakeResults = data.features;
+		  var coordinates = {};
+
+		  quakeResults.forEach(function(quake) {
+		  // console.log(quake.geometry.coordinates[0] + " " + quake.geometry.coordinates[1]);
+		  var lat = quake.geometry.coordinates[0];
+		  var lng = quake.geometry.coordinates[1];
+		  coordinates.lat = lat;
+		  coordinates.lng = lng;
+		  console.log(coordinates.lat + " " + coordinates.lng);
+		  var marker = new google.maps.Marker({
+			    position: coordinates,
+			    map: map
+			});
+		});
+
+	});
+
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 1
+        });
+	}
+
+	initMap();
+		 
+ });
+
+
+
+
+
+
